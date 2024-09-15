f0e9b55d4eec4250d4b6300834b850c9141af9de
Shankara21 <lazuardit21@gmail.com>
2024-09-15 19:13:59 +0700

    Refactor routes to use LandingPageController for home page and related routes

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
@@ -13,7 +15,35 @@ class LandingPageController extends Controller
 
     public function catalog()
     {
-        return view('pages.catalog');
+        $catalogItems = [];
+        for ($i = 1; $i <= 100; $i++) {
+            $catalogItems[] = [
+                'image_url' => 'https://via.placeholder.com/150?text=Image+' . $i,
+                'name' => 'Product ' . $i,
+                'price' => rand(10000, 100000)
+            ];
+        }
+
+        $catalogItems = collect($catalogItems);
+
+        // Ambil current page dari query string, default adalah 1
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+
+        // Tentukan berapa item per halaman (misalnya 10)
+        $perPage = 12;
+
+        // Potong data sesuai dengan halaman saat ini
+        $currentPageItems = $catalogItems->slice(($currentPage - 1) * $perPage, $perPage)->values();
+
+        // Buat LengthAwarePaginator
+        $paginatedItems = new LengthAwarePaginator(
+            $currentPageItems, // Item di halaman saat ini
+            $catalogItems->count(), // Total item
+            $perPage, // Item per halaman
+            $currentPage, // Halaman saat ini
+            ['path' => request()->url(), 'query' => request()->query()] // URL dan query string
+        );
+        return view('pages.catalog', ['catalogItems' => $paginatedItems]);
     }
 
     public function simpanPinjam()
